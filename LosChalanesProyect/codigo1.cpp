51ff8100023c919354e5dd692c5ab91c44496c83
Brayan183s <Brayangalvan2904@gmail.com>
2026-03-23 15:36:42 -0600

    commit1
    Se creó el programa base

---
+++ LosChalanesProyect/codigo1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 0;
+    int b = 0;
+
+    cin >> a >> b;
+    int sum = 0;
+
+    sum = a + b;
+
+    cout << "El resultado es: " << sum;
+
+    return 0;
+
+}
