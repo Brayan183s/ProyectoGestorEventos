00c5d9e5fce0ab4048830f49f0ea9a698cfc0f7a
Brayan183s <Brayangalvan2904@gmail.com>
2026-03-25 08:55:44 -0600

    Librería MySql
    Se agregó la librería MySql.Data en la carpeta bin\debug

--- Proyecto_Eventos_Culturales/GestorDeEventosCulturales/GestorDeEventosCulturales/ConexionBD.cs
+++ Proyecto_Eventos_Culturales/GestorDeEventosCulturales/GestorDeEventosCulturales/ConexionBD.cs
@@ -7,13 +7,14 @@ using MySql.Data.MySqlClient;
 namespace GestorDeEventosCulturales
 {
     public class ConexionBD
-{
-    private string cadena = "server=localhost;database=eventos_culturales;user=root;password=1234;";
-
-    public MySqlConnection ObtenerConexion()
     {
-        MySqlConnection conexion = new MySqlConnection(cadena);
-        conexion.Open();
-        return conexion;
+        private string cadena = "server=localhost;database=eventos_culturales;user=root;password=1234;";
+
+        public MySqlConnection ObtenerConexion()
+        {
+            MySqlConnection conexion = new MySqlConnection(cadena);
+            conexion.Open();
+            return conexion;
+        }
     }
 }
