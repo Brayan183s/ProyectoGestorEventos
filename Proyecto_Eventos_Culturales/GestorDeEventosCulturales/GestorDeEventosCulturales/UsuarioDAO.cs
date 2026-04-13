7a0e2de748dcc0b8c6f050c18ca316f882d4321a
mario254-ra <marioalr2412@gmail.com>
2026-04-13 16:09:01 -0600

    se actualizo, class usuarioDAO
    se agrego codigo en la clase UsuarioDAO

--- Proyecto_Eventos_Culturales/GestorDeEventosCulturales/GestorDeEventosCulturales/UsuarioDAO.cs
+++ Proyecto_Eventos_Culturales/GestorDeEventosCulturales/GestorDeEventosCulturales/UsuarioDAO.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MySql.Data.MySqlClient;
 
 namespace GestorDeEventosCulturales
 {
     class UsuarioDAO
     {
+        public bool ValidarUsuario(string nombre, string contrasena)
+        {
+            var con = Conexion.ObtenerConexion();
+
+            string query = "SELECT * FROM Usuario WHERE nombre=@n AND contrasena=@c";
+            MySqlCommand cmd = new MySqlCommand(query, con);
+
+            cmd.Parameters.AddWithValue("@n", nombre);
+            cmd.Parameters.AddWithValue("@c", contrasena);
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            bool existe = reader.HasRows;
+
+            reader.Close();
+            con.Close();
+
+            return existe;
+        }
     }
 }
+
