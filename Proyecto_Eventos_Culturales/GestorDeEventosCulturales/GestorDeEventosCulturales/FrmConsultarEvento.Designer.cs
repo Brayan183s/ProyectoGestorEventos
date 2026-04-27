33d15ab50daf57a118c57eb4529f0e9ac36dc1d7
Brayan183s <Brayangalvan2904@gmail.com>
2026-04-26 21:36:25 -0600

    DLL y Filtrado de eventos
    se actualizó el MySQLdata a la versión 6.4.4 así solucionando errores al consultar a la base de datos eventos en un rango de precios y fechas

--- Proyecto_Eventos_Culturales/GestorDeEventosCulturales/GestorDeEventosCulturales/FrmConsultarEvento.Designer.cs
+++ Proyecto_Eventos_Culturales/GestorDeEventosCulturales/GestorDeEventosCulturales/FrmConsultarEvento.Designer.cs
@@ -78,6 +78,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(1141, 752);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnVerDetalle);
